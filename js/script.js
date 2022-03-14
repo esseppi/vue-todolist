8a77a67475d466ce251d9b0d4d283365a1e060de
esseppidefi <97457479+esseppidefi@users.noreply.github.com>
2022-03-14 15:45:24 +0100

    start

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const app = new Vue({
+  el: '.app-container',
+  data: {
+    activeIndex: 0,
+    taskList: [
+      {
+        task: 'Do boolean exercise',
+        status: 'to-do',
+      },
+      {
+        task: 'Do boolean exercise',
+        status: 'to-do',
+      },
+      {
+        task: 'Do boolean exercise',
+        status: 'to-do',
+      },
+      {
+        task: 'Do boolean exercise',
+        status: 'to-do',
+      },
+    ],
+  },
+  // methods: {
+  // },
+});
